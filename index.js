f58fcbcb57b5f8ca5b0ab00a62b86c8ba2a82023
Christian <christian@stiqr.co>
2018-08-11 00:03:38 +0200

    Updating package.json (npm audit fix --force); introducing eslint in standard style; still many files to fix ...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-/*     
+/*
  * 	  Copyright 2015 Jens Schyma jeschyma@gmail.com
- *		
+ *
  *	  This File is a Part of the source of Open-Fin-TS-JS-Client.
  *
  *    This program is free software: you can redistribute it and/or  modify
@@ -16,6 +16,6 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *	  Please contact Jens Schyma if you are interested in a commercial license.
- *	
+ *
  */
-module.exports = require('./lib/FinTSClient.js');
+module.exports = require('./lib/FinTSClient.js')
